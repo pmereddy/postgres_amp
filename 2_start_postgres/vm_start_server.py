9394e4cd9e2a0b33da8935fdf25438540e2f6d1c
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-15 14:41:31 -0500

    fix

--- 2_start_postgres/vm_start_server.py
+++ 2_start_postgres/vm_start_server.py
@@ -6,8 +6,8 @@ extra_options=f"-k /tmp -p {port}"
 
 command=f"/usr/lib/postgresql/12/bin/initdb -D $HOME/postgresql"
 print(command)
-print(subprocess.run([command], shell=True))
+print(subprocess.run([command], shell=False))
 
 command=f"/usr/lib/postgresql/12/bin/pg_ctl -D $HOME/postgresql -l logfile -o \"{extra_options}\" start"
 print(command)
-print(subprocess.run([command], shell=True))
+print(subprocess.run([command], shell=False))
