8f27dced791cee902365b6a55c5821f88c57ef44
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-15 14:23:43 -0500

    fix

--- 2_start_postgres/vm_start_server.py
+++ 2_start_postgres/vm_start_server.py
@@ -4,6 +4,10 @@ import os
 port = str(os.environ.get('CDSW_APP_PORT', 8000))
 extra_options=f"-k /tmp -p {port}"
 
-command=f"/usr/lib/postgresql/12/bin/pg_ctl -D $HOME/postgresql/data -l logfile -o \"{extra_options}\" start"
+command=f"/usr/lib/postgresql/12/bin/initdb -D $HOME/postgresql"
+print(command)
+print(subprocess.run([command], shell=True))
+
+command=f"/usr/lib/postgresql/12/bin/pg_ctl -D $HOME/postgresql -l logfile -o \"{extra_options}\" start"
 print(command)
 print(subprocess.run([command], shell=True))
