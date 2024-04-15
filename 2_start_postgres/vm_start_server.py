0fc1ae51676989cd65c621523bebca97b6053c64
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-15 13:54:39 -0500

    fix

--- 2_start_postgres/vm_start_server.py
+++ 2_start_postgres/vm_start_server.py
@@ -1,10 +1,9 @@
 import subprocess
 import os
-import bcrypt
 
 port = str(os.environ.get('CDSW_APP_PORT', 8000))
 extra_options=f"-k /tmp -p {port}"
 
 command=f"pg_ctl -D postgres -l logfile -o \"{extra_options}\" start"
-
-print(subprocess.run([command], shell=True)
+print(command)
+print(subprocess.run([command], shell=True))
