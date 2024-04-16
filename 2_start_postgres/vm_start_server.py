c1a94fb1da84a9088ea72c44cd96b5d072f2bdba
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-16 18:23:42 -0500

    testing

--- 2_start_postgres/vm_start_server.py
+++ 2_start_postgres/vm_start_server.py
@@ -3,29 +3,24 @@ import os
 import gradio
 
 def uppercase(text):
-    return text.upper()
+    return (text, text.upper())
 
 def main():
     # Move this to configure step
     print(subprocess.run(["/usr/lib/postgresql/16/bin/initdb","-D","/home/cdsw/postgresql" ], shell=False))
 
-    print(subprocess.run(["/usr/lib/postgresql/16/bin/pg_ctl","-D","/home/cdsw/postgresql", "-o", "\"-k /tmp \"" , "start"], shell=False))
+    print(subprocess.run(["/usr/lib/postgresql/16/bin/pg_ctl","-D","/home/cdsw/postgresql", "-o", '"-k /tmp -c listen_addresses=*"' , "start"], shell=False))
 
     # Configure gradio QA app 
-    print("Configuring gradio app")
+    print("Configuring dummy gradio app")
     demo = gradio.Interface(fn=uppercase, 
                             inputs=gradio.Textbox(label="Question", placeholder=""),
                             outputs=[gradio.Textbox(label="Asking LLM with No Context"),
                                      gradio.Textbox(label="Asking LLM with Context (RAG)")],
-                            examples=["What are ML Runtimes?",
-                                      "What kinds of users use CML?",
-                                      "How do data scientists use CML?",
-                                      "What are iceberg tables?"],
                             allow_flagging="never")
 
 
     # Launch gradio app
-    print("Launching gradio app")
     demo.launch(share=True,
                 show_error=True,
                 server_name='127.0.0.1',
