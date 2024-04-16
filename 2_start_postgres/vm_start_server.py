fd889b1ce66bb4e4c0ab6336511e7bbeb91db42a
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-16 11:13:49 -0500

    fixes

--- 2_start_postgres/vm_start_server.py
+++ 2_start_postgres/vm_start_server.py
@@ -3,7 +3,8 @@ import os
 import gradio as gr
 
 port = str(os.environ.get('CDSW_APP_PORT', 8000))
-extra_options=f"-k /tmp -p {port}"
+extra_options=f"-k /tmp "
+#extra_options=f"-k /tmp -p {port}"
 
 command=f"/usr/lib/postgresql/12/bin/initdb -D $HOME/postgresql"
 print(command)
