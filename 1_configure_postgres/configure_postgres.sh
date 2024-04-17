afca443ccc9d301ffd72478146c441d0909cdd90
Pramodh Mereddy <pramodh.mereddy@gmail.com>
2024-04-16 20:26:16 -0500

    testing

--- 1_configure_postgres/configure_postgres.sh
+++ 1_configure_postgres/configure_postgres.sh
@@ -3,3 +3,4 @@
 echo "Configuring postgresql"
 echo "unix_socket_directories='/tmp'" >> /home/cdsw/postgresql/postgresql.conf
 echo "listen_addresses='*'" >> /home/cdsw/postgresql/postgresql.conf
+echo "host    all    cdsw    0/0    trust" >> /home/cdsw/postgresql/pg_hba.conf
